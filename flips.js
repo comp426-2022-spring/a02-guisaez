caf03829bb11b0a87fae88dfe8e762bc611fa4c0
Guillermo Saez <guisaez@ad.emaill.unc.edu>
2022-02-15 15:54:26 -0500

    Count Flips

--- flips.js
+++ flips.js
@@ -1,13 +1,14 @@
 import { coinFlips } from "./modules/coin.mjs";
 import { createRequire } from "module";
+import { countFlips } from "./modules/coin.mjs";
 
 const require = createRequire(import.meta.url);
 var args = require('minimist')(process.argv.slice(2));
 args['number']
 
 if(args.number == null) {
-    console.log(coinFlips(1));
+    console.log(countFlips(coinFlips(1)));
 }
 else{
-    console.log(coinFlips(args.number))
+    console.log(countFlips(coinFlips(args.number)))
 }
